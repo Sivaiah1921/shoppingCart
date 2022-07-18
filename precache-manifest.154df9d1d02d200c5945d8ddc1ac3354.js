be84ebf002cf458d3ed428744036e4af5ea864ed
Sivaiah Kothapeta <sivaiah.kothapeta@gspann.com>
2022-07-18 13:20:39 +0530

    Updates

---
+++ precache-manifest.154df9d1d02d200c5945d8ddc1ac3354.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1bc41a29cb16ebf98c7872ca7f530e1",
+    "url": "/shoppingCart/index.html"
+  },
+  {
+    "revision": "9ad49fa839e8faa75a9d",
+    "url": "/shoppingCart/static/css/2.5dcc52c6.chunk.css"
+  },
+  {
+    "revision": "b0d424c3f6a0048e6ffe",
+    "url": "/shoppingCart/static/css/main.27f54d9a.chunk.css"
+  },
+  {
+    "revision": "9ad49fa839e8faa75a9d",
+    "url": "/shoppingCart/static/js/2.1c385648.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shoppingCart/static/js/2.1c385648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0d424c3f6a0048e6ffe",
+    "url": "/shoppingCart/static/js/main.2411142e.chunk.js"
+  },
+  {
+    "revision": "57ffe160d646da7e56c2",
+    "url": "/shoppingCart/static/js/runtime-main.b8619c85.js"
+  },
+  {
+    "revision": "d6daa5c780119eb1838855a909db2c52",
+    "url": "/shoppingCart/static/media/arrow-down.d6daa5c7.svg"
+  },
+  {
+    "revision": "3a2680a80df39f16e32873d0ebd49f6f",
+    "url": "/shoppingCart/static/media/arrow-up.3a2680a8.svg"
+  },
+  {
+    "revision": "7a46c66fc5fa09517cc128b1e6f1b1d2",
+    "url": "/shoppingCart/static/media/edit-2.7a46c66f.svg"
+  },
+  {
+    "revision": "97d6cf91d790154270b4d9fea668536d",
+    "url": "/shoppingCart/static/media/facebook.97d6cf91.svg"
+  },
+  {
+    "revision": "d56ceb1edeec086d06de71a0d91aa666",
+    "url": "/shoppingCart/static/media/heart.d56ceb1e.svg"
+  },
+  {
+    "revision": "7842a82059018535059c33524550f0e9",
+    "url": "/shoppingCart/static/media/instagram.7842a820.svg"
+  },
+  {
+    "revision": "907de1ca2f6385256d2cf538402444c8",
+    "url": "/shoppingCart/static/media/menu.907de1ca.svg"
+  },
+  {
+    "revision": "c899009ad19260b626369df2d9650d68",
+    "url": "/shoppingCart/static/media/search.c899009a.svg"
+  },
+  {
+    "revision": "76b454ccf5847ba695de4ec6f5bdcf7c",
+    "url": "/shoppingCart/static/media/share-2.76b454cc.svg"
+  },
+  {
+    "revision": "1229a68cc023392e394833c72577c6c4",
+    "url": "/shoppingCart/static/media/shopping-bag.1229a68c.svg"
+  },
+  {
+    "revision": "96ce117b3d1f390c49d5143861a5183a",
+    "url": "/shoppingCart/static/media/sliders.96ce117b.svg"
+  },
+  {
+    "revision": "e835055f771af6745958f18f1cfe7ceb",
+    "url": "/shoppingCart/static/media/trash-2.e835055f.svg"
+  },
+  {
+    "revision": "dbdcf7aa861b8337fc29d31516fc1be2",
+    "url": "/shoppingCart/static/media/twitter.dbdcf7aa.svg"
+  },
+  {
+    "revision": "c28dc7cadbda821820b8e8777376ad97",
+    "url": "/shoppingCart/static/media/user.c28dc7ca.svg"
+  }
+]);
